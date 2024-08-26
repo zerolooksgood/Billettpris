6140154d4ebf20e7cbbde4ac4ec3f87a5ff0e92d
mariu <mariu@SKOLEPC_MARIUS>
2024-08-26 15:24:10 +0200

    Add project files.

---
+++ Billettpris/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billettpris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            func("Person1",15);
+            func("Person2",31);
+            func("Person3",63);
+            Console.ReadLine();
+        }
+        
+        //Oppg 1
+        static void func(string navn, int alder)
+        {
+            //Oppg 2
+            int billettpris = 0;
+            if (alder <= 17)
+            {
+                billettpris = 30;
+            } else if (alder >= 63)
+            {
+                billettpris = 35;
+            } else
+            {
+                billettpris = 50;
+            }
+            Console.WriteLine($"{navn} må betale {billettpris}kr for billetten sin");
+        }
+    }
+}
